95fb12306639b4a13573ed15c18a37ca53526214
hooman <behnejad@comp.iust.ac.ir>
2016-08-02 12:40:07 +0430

    login and signup ui completed.

--- js/views/base.js
+++ js/views/base.js
@@ -2,6 +2,11 @@
  * Created by hooman on 7/26/16.
  */
 
-$('#sad').popup({
-    position : 'bottom right'
+$('#loginPopup').popup({
+    transition : 'fade',
+    duration   : 350,
+    position   : 'bottom center',
+    on         : 'click',
+    context: "#loginPop"
 });
+$('.menu .item').tab();
