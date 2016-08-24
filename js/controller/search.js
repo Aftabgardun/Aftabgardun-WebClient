3730dd89400ff5f500092d05e41d7ef03b55117d
hooman <behnejad@comp.iust.ac.ir>
2016-08-24 16:18:11 +0430

    paper search api completed.

--- js/controller/search.js
+++ js/controller/search.js
@@ -2,28 +2,62 @@
  * Created by hooman on 8/24/16.
  */
 var cat, isUp = false;
+var icons = {
+    paper: 'large file outline icon',
+    book: 'large book icon',
+    trans: 'large flag icon',
+    null: 'large file outline icon',
+    undifined: 'large file outline icon'
+};
 function failSearch(message) {
     makeMessage('error', '#searchText', '', message);
 }
 function successSearch(data) {
+    if (data.results.length == 0) {
+        makeMessage('warning', '#searchText', '', 'موردی یافت نشد');
+        return;
+    }
+    $('#searchText').transition('fade');
+    $('#searchResult').html('').addClass('segment');
+    if (!isUp) {
+        $('#titleText').transition('fade up');
+        $('#searchUi').css('padding-top', '70px');
+        isUp = true;
+    }
     if (cat == 'person') {
-        if (data.results.length == 0) {
-            makeMessage('warning', '#searchText', '', 'موردی یافت نشد');
-            return;
+        data = data.results;
+        for (var i in data) {
+            $('#searchResult').append(
+                $('<div class="item"></div>')
+                    .append('<i class="large github middle aligned icon"></i>')
+                    .append(
+                        $('<div class="content"></div>').append(
+                            $('<a class="header"></a>').html(data[i].name).attr('href', 'person.html?id=' + data[i].personid)
+                        ).append(
+                            $('<div class="description"></div>').html(data[i].occupation)
+                        )
+                    )
+            );
         }
-        else {
-            if (!isUp) {
-                $('#titleText').transition('fade up');
-                $('#searchUi').css('padding-top', '70px');
-                isUp = true;
+    }
+    else if (cat == 'paper') {
+        data = data.results;
+        for (var i in data) {
+            var coAuthors = '';
+            for (var j in data[i].authors) {
+                coAuthors += '<a href="?id=' + data[i].authors[j].personid + '">' + data[i].authors[j].name + '</a>, '
             }
-            $('#searchResult').html('').addClass('segment');
-            data = data.results;
-            for (var i in data) {
-                $('#searchResult').append($('<div class="item"></div>').append('<i class="large github middle aligned icon"></i>')
-                    .append($('<div class="content"></div>').append($('<a class="header"></a>').html(data[i].name)
-                        .attr('href', 'person.html?id=' + data[i].personid)).append($('<div class="description"></div>').html(data[i].occupation))));
-            }
+            $('#searchResult').append(
+                $('<div class="item"></div>').append(
+                    $('<i></i>').addClass(icons[data[i].publicationtype])
+                ).append(
+                    $('<div class="content"></div>').append(
+                        $('<a class="header"></a>').html(data[i].title).attr('href', 'papers.html?id=' + data[i].paperid)
+                    ).append(
+                        $('<div class="description">').html(data[i].keywords + '<br>' + coAuthors)
+                    ).css('padding-right', '10px')
+                )
+            );
         }
     }
 }
--- js/views/personView.js
+++ js/views/personView.js
@@ -1,7 +1,13 @@
 /**
  * Created by hooman on 8/24/16.
  */
-
+var icons = {
+    paper: 'large file outline icon',
+    book: 'large book icon',
+    trans: 'large flag icon',
+    null: 'large file outline icon',
+    undifined: 'large file outline icon'
+};
 function failSearch(message) {
     makeMessage('error', '#personText', '', message);
 }
@@ -21,7 +27,28 @@ function showPersonDetail(data) {
         $('#personName').html(data.name);
     if (data.webpages.length) {
         for (var i in data.webpages) {
-            $('#homePage').append($('<a></a>').html(data.webpages[i]).attr('href', data.webpages[i]));
+            $('#homePage').append(
+                $('<a></a>').html(data.webpages[i] + '<br>').attr('href', data.webpages[i])
+            );
+        }
+    }
+    if (data.papers) {
+        for (var i in data.papers) {
+            var coAuthors = '';
+            for (var j in data.papers[i].authors) {
+                coAuthors += '<a href="?id=' + data.papers[i].authors[j].personid + '">' + data.papers[i].authors[j].name + '</a>, '
+            }
+            $('#papers').append(
+                $('<div class="item"></div>').append(
+                    $('<i></i>').addClass(icons[data.papers[i].publicationtype])
+                ).append(
+                    $('<div class="content"></div>').append(
+                        $('<a class="header"></a>').html(data.papers[i].title).attr('href', 'papers.html?id=' + data.papers[i].paperid)
+                    ).append(
+                        $('<div class="description">').html(data.papers[i].keywords + '<br>' + coAuthors)
+                    ).css('padding-right', '10px')
+                )
+            );
         }
     }
 }
